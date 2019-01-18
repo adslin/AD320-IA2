97501f49278db7f8ad8ed5f6d786becfc4f31706
adslin <shihpei.ling@gmail.com>
2019-01-18 00:18:08 -0800

    php form

---
+++ form.php
@@ -0,0 +1,9 @@
+<?php
+{
+	echo "<h1>Your input</h1>";	
+	echo "Name: " . $_GET['name'] . "</br>";
+	echo "Email: " . $_GET["email"] . "</br>";
+	echo "Phone: " . $_GET["phone"] . "</br>";
+	echo "State: " . $_GET["state"] . "</br>";
+}
+?>
