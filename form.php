4b8bcb94e9003555a2b7bd42443f59e5ccac6e10
adslin <46364113+adslin@users.noreply.github.com>
2019-01-19 23:53:33 -0800

    Update form.php

--- form.php
+++ form.php
@@ -1,9 +1,8 @@
+<!-- The server has processed your input and returned this answer. -->
 <?php
-{
-	echo "<h1>Your input</h1>";	
-	echo "Name: " . $_GET['name'] . "</br>";
-	echo "Email: " . $_GET["email"] . "</br>";
-	echo "Phone: " . $_GET["phone"] . "</br>";
-	echo "State: " . $_GET["state"] . "</br>";
-}
+	echo "<h1>Your input</h1><br />";	
+	echo "Name: " . $_GET['name'] . "<br />";
+	echo "Email: " . $_GET["email"] . "<br />";
+	echo "Phone: " . $_GET["phone"] . "<br />";
+	echo "State: " . $_GET["state"] . "<br />";
 ?>
